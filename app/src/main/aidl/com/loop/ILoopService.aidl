a543344d8248442ac396dd9a0e4dfbeaaa9b374d
SISA\hanlu.feng <hanlu.feng@samsungpay.com>
2018-01-26 11:21:09 -0500

    refactor code

--- app/src/main/aidl/com/loop/ILoopService.aidl
+++ app/src/main/aidl/com/loop/ILoopService.aidl
@@ -1,11 +1,11 @@
 
-package com.looppay;
-import com.looppay.data.IBindDeviceCallback;
-interface ILoopPayService {
+package com.loop;
+import com.loop.data.IBindDeviceCallback;
+interface ILoopService {
    /**
      * accessToken - Bind the device using this accessToken.
      * deviceCertifcate - device public certificate which is used to sign the accessToken.
      * IBindDeviceCallback is used to update the device info.
      */
-    int bindDevice( String accessToken, in byte[] deviceCertifcate,  IBindDeviceCallback callback);
+    int bindDevice(in String accessToken, in byte[] deviceCertifcate, in IBindDeviceCallback callback);
 }
--- app/src/main/java/com/client/MainActivity.kt
+++ app/src/main/java/com/client/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.client
 
-import android.Manifest
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.IBinder
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import com.looppay.ILoopPayService
-import com.looppay.data.DeviceResult
-import com.looppay.data.IBindDeviceCallback
+import com.loop.ILoopService
+import com.loop.data.DeviceResult
+import com.loop.data.IBindDeviceCallback
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var TAG: String? = "Client MainActivity";
-    var mILoopPayService: ILoopPayService?=null
+    var mILoopService: ILoopService?=null
     var mIBindCallBack: IBindDeviceCallback? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +34,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun attemptToBindService() {
         val intent = Intent()
-        intent.action = "com.looppay.aidl"
-        intent.setPackage("com.looppay");
+        intent.action = "com.loop.aidl"
+        intent.setPackage("com.loop");
         bindService(intent,mServiceConnection, Context.BIND_AUTO_CREATE)
 
     }
@@ -48,13 +44,13 @@ class MainActivity : AppCompatActivity() {
 
     var mServiceConnection: ServiceConnection = object: ServiceConnection{
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            mILoopPayService = ILoopPayService.Stub.asInterface(service)
-            if(null ==  mILoopPayService ) {
-                Log.e(TAG, "mILoopPayService is null,Service is cann't bind")
+            mILoopService = ILoopService.Stub.asInterface(service)
+            if(null == mILoopService) {
+                Log.e(TAG, "mILoopService is null,Service is cann't bind")
 
             }else {
-                Log.i(TAG, "mILoopPayService is not null")
-                mILoopPayService!!.bindDevice("afdsafsafsadfsadf", null, object : IBindDeviceCallback.Stub() {
+                Log.i(TAG, "mILoopService is not null")
+                mILoopService!!.bindDevice("afdsafsafsadfsadf", null, object : IBindDeviceCallback.Stub() {
                     override fun onSuccess(result: DeviceResult?) {
                         if (null != result)
                             Log.i(TAG, "IBindDeviceCallback: " + "deviceName: " + result.name + "  deviceBrand: " + result.brand)
